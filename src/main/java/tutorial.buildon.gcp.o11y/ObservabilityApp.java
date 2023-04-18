ef86c47bdd4e5d4ecdd13ac78c570659c4d273cc
owensulei3841 <33892816+owensulei3841@users.noreply.github.com>
2023-04-17 19:48:43 -0700

    Add files via upload

---
+++ src/main/java/tutorial.buildon.gcp.o11y/ObservabilityApp.java
@@ -0,0 +1,13 @@
+package tutorial.buildon.gcp.o11y;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilityApp.class, args);
+	}
+
+}
