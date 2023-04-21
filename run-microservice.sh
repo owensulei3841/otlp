063ad64e1eb2c0c5bdcbf6bfd2ed65a1f310d1e2
owensulei3841 <33892816+owensulei3841@users.noreply.github.com>
2023-04-21 14:15:12 -0700

    Add files via upload

--- run-microservice.sh
+++ run-microservice.sh
@@ -2,15 +2,18 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-AGENT_FILE=opentelemetry-javaagent-all.jar
+OTELCOL_FILE=otelcol
+OTELCOL_CONTRIB_FILE=otelcol-contrib
+  wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.75.0/otelcol_0.75.0_linux_amd64.tar.gz
+  wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.75.0/otelcol-contrib_0.75.0_linux_amd64.tar.gz
+  tar -zxvf otelcol_0.75.0_linux_amd64.tar.gz
+  tar -zxvf otelcol-contrib_0.75.0_linux_amd64.tar.gz
 
-if [ ! -f "${AGENT_FILE}" ]; then
-  curl -L https://github.com/aws-observability/aws-otel-java-instrumentation/releases/download/v1.19.2/aws-opentelemetry-agent.jar --output ${AGENT_FILE}
-fi
 
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=otlp
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:5555
 export OTEL_RESOURCE_ATTRIBUTES=service.name=observability-poc,service.version=1.0
 
-java -javaagent:./${AGENT_FILE} -jar target/observability-poc-1.0.jar
+java -jar target/observability-poc-1.0.jar
+chmod +x otelcol-contrib
+./otelcol-contrib --config=config.yaml
